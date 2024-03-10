e4e32b291d7e3dc397860cc405390b6cc88c67b1
Rain Leung <rainleung0218@gmail.com>
2024-03-10 11:28:15 +0800

    Add DT classifier and regressor

--- supervised_learning/decision_tree.py
+++ supervised_learning/decision_tree.py
@@ -16,11 +16,10 @@ class DecisionNode:
 
 class DecisionTree:
     """
-    A decision tree classifier.
+    The base class of decision tree.
     """
 
-    def __init__(self, criterion="gini", max_depth=2, min_samples_split=2):
-        self.criterion = criterion
+    def __init__(self, max_depth=2, min_samples_split=2):
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
 
@@ -33,10 +32,10 @@ class DecisionTree:
         n_samples = X.shape[0]
 
         if depth >= self.max_depth or n_samples < self.min_samples_split:
-            leaf_value = self.get_most_common(y)
+            leaf_value = self.get_leaf_value(y)
             return DecisionNode(value=leaf_value)
 
-        feature_index, threshold = self.best_criteria(X, y)
+        feature_index, threshold = self.get_best_criteria(X, y)
         left_indices, right_indices = self.split(X[:, feature_index], threshold)
 
         left_tree = self.build_tree(X[left_indices, :], y[left_indices], depth + 1)
@@ -44,8 +43,11 @@ class DecisionTree:
 
         return DecisionNode(feature_index, threshold, left_tree, right_tree)
 
-    def best_criteria(self, X, y):
-        max_info_gain = float("-inf")
+    def get_leaf_value(self, y):
+        raise Exception("Implemented in subclass")
+
+    def get_best_criteria(self, X, y):
+        max_impurity = float("-inf")
         best_feature_index, best_threshold = None, None
 
         for feature_index in range(X.shape[1]):
@@ -58,10 +60,10 @@ class DecisionTree:
                 if len(left_indices) == 0 or len(right_indices) == 0:
                     continue
 
-                info_gain = self.get_info_gain(y, y[left_indices], y[right_indices], self.criterion)
+                impurity = self.get_impurity(y, y[left_indices], y[right_indices])
 
-                if info_gain > max_info_gain:
-                    max_info_gain = info_gain
+                if impurity > max_impurity:
+                    max_impurity = impurity
                     best_feature_index = feature_index
                     best_threshold = threshold
 
@@ -80,35 +82,8 @@ class DecisionTree:
 
         return left_indices, right_indices
 
-    def get_info_gain(self, y, left_y, right_y, criterion):
-        n = len(y)
-        p_left = len(left_y) / n
-        p_right = len(right_y) / n
-
-        if criterion == "gini":
-            return self.gini(y) - (p_left * self.gini(left_y) + p_right * self.gini(right_y))
-        elif criterion == "entropy":
-            return self.entropy(y) - (p_left * self.entropy(left_y) + p_right * self.entropy(right_y))
-        else:
-            raise ValueError(f"Unknown criterion: {criterion}")
-
-    def entropy(self, y):
-        counts = np.unique(y, return_counts=True)[1]
-        prob = counts / len(y)
-
-        return -np.sum(prob * np.log2(prob))
-
-    def gini(self, y):
-        counts = np.unique(y, return_counts=True)[1]
-        prob = counts / len(y)
-
-        return 1 - np.sum(prob ** 2)
-
-    def get_most_common(self, y):
-        if len(y) == 0:
-            return None
-
-        return np.bincount(y).argmax()
+    def get_impurity(self, y, left_y, right_y):
+        raise Exception("Implemented in subclass")
 
     def predict(self, X):
         return [self._predict(x) for x in X]
@@ -137,3 +112,74 @@ class DecisionTree:
             print(f"{depth * '  '}{node.feature_index} <= {node.threshold}")
             self.print_tree(node.left, depth + 1)
             self.print_tree(node.right, depth + 1)
+
+
+class DecisionTreeClassifier(DecisionTree):
+    """
+    A decision tree classifier.
+    """
+
+    def __init__(self, criterion="gini", max_depth=2, min_samples_split=2):
+        super().__init__(max_depth, min_samples_split)
+        self.criterion = criterion
+
+    def get_leaf_value(self, y):
+        if len(y) == 0:
+            return None
+
+        return np.bincount(y).argmax()
+
+    def get_impurity(self, y, left_y, right_y):
+        n = len(y)
+        p_left = len(left_y) / n
+        p_right = len(right_y) / n
+
+        if self.criterion == "gini":
+            return self.gini(y) - (p_left * self.gini(left_y) + p_right * self.gini(right_y))
+        elif self.criterion == "entropy":
+            return self.entropy(y) - (p_left * self.entropy(left_y) + p_right * self.entropy(right_y))
+        else:
+            raise ValueError(f"Unknown criterion: {self.criterion}")
+
+    def entropy(self, y):
+        counts = np.unique(y, return_counts=True)[1]
+        prob = counts / len(y)
+
+        return -np.sum(prob * np.log2(prob))
+
+    def gini(self, y):
+        counts = np.unique(y, return_counts=True)[1]
+        prob = counts / len(y)
+
+        return 1 - np.sum(prob ** 2)
+
+
+class DecisionTreeRegressor(DecisionTree):
+    """
+    A decision tree regressor.
+    """
+
+    def __init__(self, criterion="mse", max_depth=2, min_samples_split=2):
+        super().__init__(max_depth, min_samples_split)
+        self.criterion = criterion
+
+    def get_leaf_value(self, y):
+        return np.mean(y)
+
+    def get_impurity(self, y, left_y, right_y):
+        n = len(y)
+        p_left = len(left_y) / n
+        p_right = len(right_y) / n
+
+        if self.criterion == "mse":
+            return self.mse(y) - (p_left * self.mse(left_y) + p_right * self.mse(right_y))
+        elif self.criterion == "mae":
+            return self.mae(y) - (p_left * self.mae(left_y) + p_right * self.mae(right_y))
+        else:
+            raise ValueError(f"Unknown criterion: {self.criterion}")
+
+    def mse(self, y):
+        return np.mean((y - np.mean(y)) ** 2)
+
+    def mae(self, y):
+        return np.mean(np.abs(y - np.mean(y)))
